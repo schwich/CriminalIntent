98c3d0f5f1d51e7c09a16dd04fcc96f9e19278b2
Jordan Schwichtenberg <jordanschwichtenberg@gmail.com>
2015-02-06 09:46:03 -0800

    Add landscape layout

--- app/src/main/java/com/jordanschwichtenberg/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/jordanschwichtenberg/criminalintent/CrimeFragment.java
@@ -9,6 +9,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 
@@ -24,6 +27,8 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -87,6 +92,20 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Set the crime's solved property.
+                mCrime.setSolved(isChecked);
+            }
+        });
+
         return v;
     }
 
